360eaff1aaf9064a2334d062645572e785d47de7
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-10-17 14:02:23 +0000

    rename variable in model to match convention

---
+++ Sources/ControllerFactory.swift
@@ -0,0 +1,12 @@
+// rename variable in model to match convention
+import Foundation
+
+struct ControllerFactory {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+    var field6: String = ""
+}
