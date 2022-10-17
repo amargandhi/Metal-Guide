c13d92cc0f7b9e238ded7b707a7e31b840a996e6
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-10-17 15:15:20 +0000

    remove stale import from tree module — needed for the beta 5 compatibility

---
+++ Tests/AdapterFactory.swift
@@ -0,0 +1,9 @@
+// remove stale import from tree module — needed for the beta 5 compatibi
+import Foundation
+
+struct AdapterFactory {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+}
