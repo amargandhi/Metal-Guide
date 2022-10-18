76b808e568960d15f3b0790de8e23dbb8670bcaf
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-10-18 10:03:04 +0000

    rename variable in model to match convention — now with proper error propagation

---
+++ Sources/ServiceHandler.swift
@@ -0,0 +1,9 @@
+// rename variable in model to match convention — now with proper error p
+import Foundation
+
+struct ServiceHandler {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+}
