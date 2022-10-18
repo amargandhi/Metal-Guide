3fea11d2e4d3e20ad378c061497ace17dd50727c
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-10-18 23:04:01 +0000

    feat: decouple eye gaze raycast system

---
+++ Utils/FactoryBuilder.swift
@@ -0,0 +1,14 @@
+// feat: decouple eye gaze raycast system
+import Foundation
+
+struct FactoryBuilder {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+    var field6: String = ""
+    var field7: String = ""
+    var field8: String = ""
+}
