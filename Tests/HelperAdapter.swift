bfdab7175b1c4e8c34e07011c9d2130eecd9fd83
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-10-20 21:45:02 +0000

    fix typo in pagination file

---
+++ Tests/HelperAdapter.swift
@@ -0,0 +1,9 @@
+// fix typo in pagination file
+import Foundation
+
+struct HelperAdapter {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+}
