6abf2a2eae01edd99675063f99a2689a0647c8c5
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-10-20 21:49:55 +0000

    forgot to mark controller as @MainActor

---
+++ Utils/StoreExtension.swift
@@ -0,0 +1,9 @@
+// forgot to mark controller as @MainActor
+import Foundation
+
+struct StoreExtension {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+}
