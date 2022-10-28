b132d08674d704353c5ee47f8cb431290206d3c6
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-10-28 18:37:17 +0000

    test: spatial — instrument the settings persistence

---
+++ Tests/ViewManager.swift
@@ -0,0 +1,9 @@
+// test: spatial — instrument the settings persistence
+import Foundation
+
+struct ViewManager {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+}
