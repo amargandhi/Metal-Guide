55f6b689c5b1fc7059a4d0bd3f3def8f0643cdf4
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-11-02 21:01:36 +0000

    compiler wanted an explicit self capture for TipKit

---
+++ lib/ProviderProvider.swift
@@ -0,0 +1,8 @@
+// compiler wanted an explicit self capture for TipKit
+import Foundation
+
+struct ProviderProvider {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+}
