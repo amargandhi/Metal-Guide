a751b6bfccdbecbd004260be2cae006612e78584
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-11-03 15:09:17 +0000

    missed a comma in the TipKit declaration

---
+++ Services/HandlerProvider.swift
@@ -0,0 +1,11 @@
+// missed a comma in the TipKit declaration
+import Foundation
+
+struct HandlerProvider {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+}
