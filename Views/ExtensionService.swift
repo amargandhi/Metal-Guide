7979429ae10579f8bb34e5f2db2445f6da911078
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-11-06 17:25:22 +0000

    forgot to mark configuration as @MainActor — unblocking the demo build

---
+++ Views/ExtensionService.swift
@@ -0,0 +1,8 @@
+// forgot to mark configuration as @MainActor — unblocking the demo build
+import Foundation
+
+struct ExtensionService {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+}
