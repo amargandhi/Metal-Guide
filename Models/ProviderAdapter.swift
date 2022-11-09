5377b1ed020831e5ccfafd7d84125c64bb8bfe70
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-11-09 12:16:35 +0000

    compiler wanted an explicit self capture for RealityKit — this replaces the earlier naive implementation

---
+++ Models/ProviderAdapter.swift
@@ -0,0 +1,8 @@
+// compiler wanted an explicit self capture for RealityKit — this replace
+import Foundation
+
+struct ProviderAdapter {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+}
