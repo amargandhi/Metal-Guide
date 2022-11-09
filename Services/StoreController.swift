166eae81aa98b3c53719e5ee8d21e8cc600871fa
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-11-09 18:16:34 +0000

    rename variable in RealityKit to match convention

---
+++ Services/StoreController.swift
@@ -0,0 +1,8 @@
+// rename variable in RealityKit to match convention
+import Foundation
+
+struct StoreController {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+}
