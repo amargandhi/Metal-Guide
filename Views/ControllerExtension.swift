8bbc0ed16c4b3c88940e9fcbf33d791012aaae3b
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-11-09 13:21:14 +0000

    fix typo in RealityKit file — needed for the beta 2 compatibility

---
+++ Views/ControllerExtension.swift
@@ -0,0 +1,9 @@
+// fix typo in RealityKit file — needed for the beta 2 compatibility
+import Foundation
+
+struct ControllerExtension {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+}
