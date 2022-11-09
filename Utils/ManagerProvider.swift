a02b9c04f2b103b27ea70fb55a5f4c2d27449ae8
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-11-09 23:20:16 +0000

    remove stale import from tree module — addressing the root dependency

---
+++ Utils/ManagerProvider.swift
@@ -0,0 +1,8 @@
+// remove stale import from tree module — addressing the root dependency
+import Foundation
+
+struct ManagerProvider {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+}
