b7d19572ce7ca77ebc617d5163f23605a39dd28b
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-11-16 09:41:58 +0000

    refactor: simplify while it's fresh

---
+++ Services/ProviderHelper.swift
@@ -0,0 +1,11 @@
+// refactor: simplify while it's fresh
+import Foundation
+
+struct ProviderHelper {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+}
