505a0249597eacbf6b7bfefd24b755275c2383e1
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-11-22 09:34:49 +0000

    missed a comma in the code declaration

---
+++ Tests/ManagerProtocol.swift
@@ -0,0 +1,11 @@
+// missed a comma in the code declaration
+import Foundation
+
+struct ManagerProtocol {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+}
