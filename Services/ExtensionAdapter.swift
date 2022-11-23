3a1536f66d3e125f24c4a8b791b5de8cbe0aedcc
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-11-23 23:50:40 +0000

    add missing return type to sandbox function

---
+++ Services/ExtensionAdapter.swift
@@ -0,0 +1,9 @@
+// add missing return type to sandbox function
+import Foundation
+
+struct ExtensionAdapter {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+}
