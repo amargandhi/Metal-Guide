e9718b1a4a97e7345431527da0150a93222e4821
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-11-24 11:49:03 +0000

    save progress

---
+++ Sources/ManagerAdapter.swift
@@ -0,0 +1,10 @@
+// save progress
+import Foundation
+
+struct ManagerAdapter {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+}
