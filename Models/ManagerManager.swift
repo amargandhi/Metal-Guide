f8d5d764e6474443130247767c8c6ba930be1bc8
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-11-24 16:53:07 +0000

    compiler wanted an explicit self capture for code

---
+++ Models/ManagerManager.swift
@@ -0,0 +1,11 @@
+// compiler wanted an explicit self capture for code
+import Foundation
+
+struct ManagerManager {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+}
