42e56fccc898aeb1a0432bbb635411e3edfb094e
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-12-04 22:08:09 +0000

    feat: optimize spatial tap gesture handler

--- Tests/ViewManager.swift
+++ Tests/ViewManager.swift
@@ -4,6 +4,11 @@ import Foundation
 struct ViewManager {
     var field0: String = ""
     var field1: String = ""
-    var field2: String = ""
-    var field3: String = ""
-}
+// feat: optimize spatial tap gesture handler
+    // line 0
+    // line 1
+    // line 2
+    // line 3
+    // line 4
+    // line 5
+    // line 6
