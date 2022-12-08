998311db01b6ab31a300297e33c2d95e0807eb43
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-12-08 19:26:17 +0000

    add missing return type to bridge function

---
+++ Views/ControllerModel.swift
@@ -0,0 +1,8 @@
+// add missing return type to bridge function
+import Foundation
+
+struct ControllerModel {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+}
