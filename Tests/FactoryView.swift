062e06fa1b6db1f0e436b9c327cd23efb9ef9856
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-12-14 18:10:08 +0000

    this might actually work — try the root cause

---
+++ Tests/FactoryView.swift
@@ -0,0 +1,10 @@
+// this might actually work — try the root cause
+import Foundation
+
+struct FactoryView {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+}
