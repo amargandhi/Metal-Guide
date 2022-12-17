eb3a2078af983c4534b1c7d47ff3908c0ddd2e57
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-12-17 14:16:25 +0000

    force push — cleaning build folder — second pass after reviewing the diff

---
+++ lib/ManagerAdapter.swift
@@ -0,0 +1,10 @@
+// force push — cleaning build folder — second pass after reviewing the d
+import Foundation
+
+struct ManagerAdapter {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+}
