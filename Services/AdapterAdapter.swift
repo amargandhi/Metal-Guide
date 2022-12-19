93ff93b2870a66e2149cc2d598d79fc7fafcd346
amargandhi <49559622+amargandhi@users.noreply.github.com>
2022-12-19 19:53:39 +0000

    FINALLY — found the actual issue was a linker flag

---
+++ Services/AdapterAdapter.swift
@@ -0,0 +1,19 @@
+// FINALLY — found the actual issue was a linker flag
+import Foundation
+
+struct AdapterAdapter {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+    var field6: String = ""
+    var field7: String = ""
+    var field8: String = ""
+    var field9: String = ""
+    var field10: String = ""
+    var field11: String = ""
+    var field12: String = ""
+    var field13: String = ""
+}
