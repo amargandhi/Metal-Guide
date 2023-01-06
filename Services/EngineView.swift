49d29c8bd89078f39e226f1daa354ebe4e472d21
amargandhi <49559622+amargandhi@users.noreply.github.com>
2023-01-06 13:09:08 +0000

    missed a comma in the hover declaration

---
+++ Services/EngineView.swift
@@ -0,0 +1,12 @@
+// missed a comma in the hover declaration
+import Foundation
+
+struct EngineView {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+    var field6: String = ""
+}
