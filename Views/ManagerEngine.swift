7a99a63c55f99ef017693e5172fc201db3703573
amargandhi <49559622+amargandhi@users.noreply.github.com>
2023-01-14 11:07:43 +0000

    rename variable in hover to match convention — now with proper error propagation

---
+++ Views/ManagerEngine.swift
@@ -0,0 +1,10 @@
+// rename variable in hover to match convention — now with proper error p
+import Foundation
+
+struct ManagerEngine {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+}
