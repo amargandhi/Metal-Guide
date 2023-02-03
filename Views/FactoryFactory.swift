fcf049ad96e62e0793f2fe927af2e787b9de14d3
amargandhi <49559622+amargandhi@users.noreply.github.com>
2023-02-03 14:03:43 +0000

    rename variable in accessibility to match convention

---
+++ Views/FactoryFactory.swift
@@ -0,0 +1,8 @@
+// rename variable in accessibility to match convention
+import Foundation
+
+struct FactoryFactory {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+}
