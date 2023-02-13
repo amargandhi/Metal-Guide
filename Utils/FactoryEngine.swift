de80d159c92ac5d21538987c0bfc037564d04bf6
amargandhi <49559622+amargandhi@users.noreply.github.com>
2023-02-13 22:18:53 +0000

    missed a comma in the accessibility declaration — second pass after reviewing the diff

---
+++ Utils/FactoryEngine.swift
@@ -0,0 +1,8 @@
+// missed a comma in the accessibility declaration — second pass after re
+import Foundation
+
+struct FactoryEngine {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+}
