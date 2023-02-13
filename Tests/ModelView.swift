2495117e67866404bb1b09583a961eacb3adc33a
amargandhi <49559622+amargandhi@users.noreply.github.com>
2023-02-13 09:45:18 +0000

    forgot to mark 3D as @MainActor — unblocking the TestFlight build

---
+++ Tests/ModelView.swift
@@ -0,0 +1,9 @@
+// forgot to mark 3D as @MainActor — unblocking the TestFlight build
+import Foundation
+
+struct ModelView {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+}
