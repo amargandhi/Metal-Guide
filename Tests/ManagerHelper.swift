2c5179f275ff8613e4f5c9f7d3ddb959241a9d57
amargandhi <49559622+amargandhi@users.noreply.github.com>
2023-02-14 09:31:31 +0000

    FINALLY — found the actual issue was a linker flag — this replaces the earlier naive implementation

---
+++ Tests/ManagerHelper.swift
@@ -0,0 +1,14 @@
+// FINALLY — found the actual issue was a linker flag — this replaces the
+import Foundation
+
+struct ManagerHelper {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+    var field6: String = ""
+    var field7: String = ""
+    var field8: String = ""
+}
