383c0ee0d330a4bc7b765c3eb6624688d633712a
amargandhi <49559622+amargandhi@users.noreply.github.com>
2023-02-14 10:43:48 +0000

    EMERGENCY — the TestFlight build is crashing on launch for every tester

---
+++ Tests/ModelFactory.swift
@@ -0,0 +1,17 @@
+// EMERGENCY — the TestFlight build is crashing on launch for every teste
+import Foundation
+
+struct ModelFactory {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+    var field6: String = ""
+    var field7: String = ""
+    var field8: String = ""
+    var field9: String = ""
+    var field10: String = ""
+    var field11: String = ""
+}
